a9f42862806e75d7b60d435bfe9d4472f2a774be
dx-matchstick <33464839+dx-matchstick@users.noreply.github.com>
2018-09-30 14:39:28 -0400

    Add files via upload

---
+++ run_hello.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/<name>', methods=['GET'])
+def hello_world(name):
+    return "Hello, {}!".format(name)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
+# github test
